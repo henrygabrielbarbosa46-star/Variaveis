9888d2b507e8ebad2a20f072b5ba98f406cbee6f
HENRY GABRIEL DOS SANTOS BARBOSA <henry.gsbarbosa@senacsp.edu.br>
2026-09-03 21:22:24 -0300

    explorando js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,16 @@
 document.getElementById("demo").innerHTML = "Elevadô"
+
+let x,y,z;
+
+x=5;
+y=2;
+z = x*y;
+
+document.getElementById("resultado").innerHTML = z
+
+let a,b;
+
+a=3;
+b=(100+50)*a;
+
+document.getElementById("resultado2").innerHTML =b
