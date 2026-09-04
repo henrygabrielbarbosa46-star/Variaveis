97d7e6010fdf8e5b900d4bc2ac903a89eb08845b
HENRY GABRIEL DOS SANTOS BARBOSA <henry.gsbarbosa@senacsp.edu.br>
2026-09-04 19:14:47 -0300

    Alterações no js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,11 +6,19 @@ x=5;
 y=2;
 z = x*y;
 
-document.getElementById("resultado").innerHTML = z
+document.getElementById("resultado").innerHTML = z;
 
 let a,b;
 
 a=3;
 b=(100+50)*a;
 
-document.getElementById("resultado2").innerHTML =b
+document.getElementById("resultado2").innerHTML =b;
+
+function pegarValor(){
+    let renan = document.getElementById("meuInput");
+
+    let ValorDigitado = renan.value;
+
+    document.getElementById("olá").innerHTML= ValorDigitado;
+}
